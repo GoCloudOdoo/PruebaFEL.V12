d4c9f9bb0d9042de8c657952a603d1aad83918f5
Osmin Cano <64631254+oimeqmo@users.noreply.github.com>
2021-04-16 00:12:15 -0600

    Update account_invoice.py

--- fel/models/account_invoice.py
+++ fel/models/account_invoice.py
@@ -8,7 +8,7 @@ import dateutil.parser
 from dateutil.tz import gettz
 from odoo.addons.fel.models import credit_note
 from odoo.addons.fel.models import invoice_cancel
-from .models import nota_abono
+from odoo.addons.fel.models import nota_abono
 import json
 from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
 import logging
